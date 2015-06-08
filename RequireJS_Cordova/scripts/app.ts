0d29eb850e0d466f15ed908eccba8ac8b9251ae5
DeanB2015 <dean@invisionlearning.com>
2015-06-07 20:03:31 -0400

    Basic VS 2015 TypeScript, Cordova, RequireJS files

---
+++ RequireJS_Cordova/scripts/app.ts
@@ -0,0 +1,14 @@
+﻿import jq = require('jquery');
+// Note "cannot compile external modules unless the "--module" flag is provided." 
+// This is resolved by adding a tsconfig.js to the root of the project.
+module App {
+    "use strict";
+    /**
+    * Initialize is used to test if jQuery will display a version number in the console
+    **/
+    export function initialize(){
+        console.log("Initialize Called. jQuery Version = " + jq.fn.jquery);
+        
+    }
+}
+export = App;
---
+++ RequireJS_Cordova/scripts/index.ts
@@ -0,0 +1,23 @@
+﻿/// <reference path='typings/requirejs/require.d.ts' />
+import App = require('./app');
+requirejs.config({
+    baseUrl: 'scripts',
+    paths: {
+        jquery: 'jquery'
+    }
+
+    //only use shim config for non- AMD scripts,
+    //scripts that do not already call define(). The shim
+    //config will not work correctly if used on AMD scripts,
+    //in particular, the exports and init config will not
+    //be triggered, and the deps config will be confusing
+    //for those cases.
+});
+requirejs(['../cordova'], () => {
+    //This function will be called when all the dependencies
+    //listed above are loaded. Note that this function could
+    //be called before the page is loaded.
+    App.initialize();
+});
+
+// More information about RequireJS can be found @ http://requirejs.org/docs/api.html
